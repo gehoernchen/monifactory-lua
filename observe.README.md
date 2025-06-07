8ea7dde1b74502d43833458e2237fc841aa8ef31
gehoernchen <geheornchen@gehoernchen.de>
2025-06-07 19:52:45 +0200

    add maximumCount functionality

--- observe.README.md
+++ observe.README.md
@@ -9,25 +9,41 @@ Create a file that looks like this and call it `observe-config`:
 UPDATE_RATE = 15
 NEWLINES = true
 CONFIG_REDSTONE_OUTPUT_SIDE = "left"
-OBSERVED = {
-    {
-        name = "gtceu:ethylbenzene",
+{
+        name = "gtceu:nitrogen_dioxide",
         minimumCount = 500000,
-        type = "fluid"
+        type = "fluid",
+        mode = "minimum"
     },
     {
-        name = "gtceu:carbon_dust",
-        minimumCount = 64,
-        type = "item"
+        name = "gtceu:deuterium",
+        minimumCount = 500000,
+        type = "fluid",
+    },
+    {
+        name = "enderio:item_conduit",
+        maximumCount = 128,
+        minimumCount = 50,
+        type = "item",
+    },
+    {
+        name = "enderio:vibrant_conduit",
+        maximumCount = 50,
+        minimumCount = 10,
+        type = "item",
     }
-}
 ```
 
 You may start with the example `observe-config` provided here.
 
+An item can have a maximumCount, minimumCount or both at the same time.
+If any item's maximum is reached, redstone is turned off. Maximum counts take precedence, i.e., even if a minimum is active, redstone output is toggled when at least one maximum is reached.
+
+Red lines denote when a minimum is fulfilled. Green lines denote a maximum having been reached.
+
 Notes:
 - Amounts for fluids are given as mB.
-- Without the configuration file, the script will not work
-- If `NEWLINES` is given, each observed item is followed by a new line, aiding readability, true by default
-- `CONFIG_REDSTONE_OUTPUT_SIDE` determines the side to output a redstone signal to, set to "back" by default
-- `UPDATE_RATE` determines how often to check the amounts in the ME in seconds and is set to 15s by default
+- Without the configuration file, the script will not work.
+- If `NEWLINES` is given, each observed item is followed by a new line, aiding readability, true by default.
+- `CONFIG_REDSTONE_OUTPUT_SIDE` determines the side to output a redstone signal to, set to "back" by default.
+- `UPDATE_RATE` determines how often to check the amounts in the ME in seconds and is set to 15s by default.
--- observe.lua
+++ observe.lua
@@ -1,5 +1,5 @@
--- VERSION 0.3
--- 2025-05-28
+-- VERSION 0.5
+-- 2025-06-07
 -- Antonia
 
 local CONFIG_FILE = "observe-config"
@@ -48,13 +48,48 @@ local function printObservedItems()
     print("Observed objects:")
     
     for _, item in pairs(OBSERVED) do
-        term.setTextColor(item["color"])
-
-        if item["type"] == "item" then
-            print(item["name"], "T:", item["minimumCount"], "A:", item["actualAmount"])
-        else
-            print(item["name"], "T:", item["minimumCount"] / 1000 .. 'B', "A:", item["actualAmount"] / 1000 .. 'B')
+        color = item["color"]
+        
+        if color == nil then
+            color = colors.white
         end
+
+        term.setTextColor(color)
+
+        minimumCountStr = "min"
+        maximumCountStr = "max"
+
+        -- build output string for this item
+        itemLine = item["name"]
+        itemLine = itemLine .. " T:"
+        
+        -- if item has minimum count, add this to string
+        if item["minimumCount"] ~= nil then
+            if item["type"] == "item" then
+                itemLine = itemLine .. " " .. minimumCountStr .. " " .. item["minimumCount"]
+            else
+                itemLine = itemLine .. " " .. minimumCountStr .. " " .. item["minimumCount"] / 1000 .. "B"
+            end
+        end
+
+        if item["maximumCount"] ~= nil then
+            if item["type"] == "item" then
+                itemLine = itemLine .. " " .. maximumCountStr .. " " .. item["maximumCount"]
+            else
+                itemLine = itemLine .. " " .. maximumCountStr .. " " .. item["maximumCount"] / 1000 .. "B"
+            end
+        end
+
+        -- now add actual amount
+        -- differentiate between item and fluid to do some maths to make buckets nicer to read
+        if item["type"] == "item" then
+            itemLine = itemLine .. " " .. "A:" .. " " .. item["actualAmount"]
+        else
+            itemLine = itemLine .. " " .. "A:" .. " " .. item["actualAmount"] / 1000 .. "B"
+        end
+
+        print(itemLine)
+
         if NEWLINES then
             print()
         end
@@ -67,39 +102,78 @@ local function observeLoop()
     require(CONFIG_FILE)
     
     while true do
+        local time = textutils.formatTime(os.time("local"), true)
+        redstoneChanged = false
         redstoneActive = true
         doLoop = true
         shell.run("clear")
 
-        -- check if there is an ME
-        if BRIDGE.listCells() == nil then
-            local time = textutils.formatTime(os.time("local"), true)
-            print(time)
-            print("ME not found, skipping...")
-            doLoop = false
-        end
-        
-        -- prepare table
+        for i = 1, #OBSERVED do
+            -- check if there is an ME
+            if BRIDGE.listCells() == nil then
+                print(time)
+                print("ME not found, skipping...")
+                doLoop = false
+                break
+            end
 
-        if doLoop then
-            for i = 1, #OBSERVED do
-                if OBSERVED[i]["type"] == "fluid" then
-                    amount = getCountFluid(OBSERVED[i]["name"])
-                elseif OBSERVED[i]["type"] == "item" then
-                    amount = getCountItem(OBSERVED[i]["name"])
+            if OBSERVED[i]["type"] == "fluid" then
+                amount = getCountFluid(OBSERVED[i]["name"])
+            elseif OBSERVED[i]["type"] == "item" then
+                amount = getCountItem(OBSERVED[i]["name"])
+            end
+
+            OBSERVED[i]["actualAmount"] = amount
+
+            if OBSERVED[i]["actualAmount"] == nil then
+                OBSERVED[i]["actualAmount"] = "N/A"
+                goto continue
+            end
+
+            -- check if the item has a minimum we must act for
+            if OBSERVED[i]["minimumCount"] ~= nil then
+                if OBSERVED[i]["actualAmount"] <= OBSERVED[i]["minimumCount"] then
+                    OBSERVED[i]["color"] = colors.red
+
+                    if redstoneChanged then
+                        goto continue
+                    end
+
+                    redstoneActive = false
+                    redstoneChanged = true
+
+                    goto continue
+                else
+                    OBSERVED[i]["color"] = colors.white
                 end
 
-                OBSERVED[i]["actualAmount"] = amount
+            end
 
-                if OBSERVED[i]["actualAmount"] < OBSERVED[i]["minimumCount"] then
-                    OBSERVED[i]["color"] = colors.red
-                    redstoneActive = false
+            -- check if the item has a maximum we must act for
+            if OBSERVED[i]["maximumCount"] ~= nil then
+                if OBSERVED[i]["actualAmount"] >= OBSERVED[i]["maximumCount"] then
+                    OBSERVED[i]["color"] = colors.green
+
+                    -- maximum takes precedence
+                    -- if redstoneChanged then
+                    --    goto continue
+                    -- end
+
+                    redstoneActive = true
+                    redstoneChanged = true
+
+                    goto continue
                 else
                     OBSERVED[i]["color"] = colors.white
                 end
             end
 
-            redstone.setOutput(CONFIG_REDSTONE_OUTPUT_SIDE, redstoneActive)
+            ::continue::
+        end
+
+        redstone.setOutput(CONFIG_REDSTONE_OUTPUT_SIDE, redstoneActive)
+        
+        if doLoop then
             printObservedItems()
         end
 
